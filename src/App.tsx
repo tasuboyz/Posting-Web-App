e9911d6cb44faee46b79a47eeeca0740cb08d199
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-07-07 16:37:53 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,16 @@ function App() {
   const [titolo, setTitolo] = React.useState('');
   const [description, setDescription] = React.useState('');
   const [tag, setTag] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [account, setAccount] = React.useState('');
 
   const inviaMessaggio = (): void => {
     const post = {
         title: titolo,
         description: description,
-        tag: tag
+        tag: tag,
+        account: account,
+        password: password
     }
     window.Telegram.WebApp.sendData(JSON.stringify(post));
 };
@@ -47,6 +51,20 @@ function App() {
         className="input-tag"
         value={tag}
         onChange={(e) => setTag(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Write here account"
+        className="input-account"
+        value={account}
+        onChange={(e) => setAccount(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Write here password"
+        className="input-password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
       {/* Bottone di invio post */}
       <button className="button" onClick={inviaMessaggio}>Invia Post</button>
