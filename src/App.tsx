a36ac6fc0d454c7b76e54eb8a1f1167c735c2735
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-07-14 17:48:35 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,46 @@ function App() {
   const [titolo, setTitolo] = React.useState('');
   const [description, setDescription] = React.useState('');
   const [tag, setTag] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [account, setAccount] = React.useState('');
+  // const [initData, setInitData] = useState('');
 
   const inviaMessaggio = (): void => {
     const post = {
         title: titolo,
         description: description,
-        tag: tag,
-        account: account,
-        password: password
+        tag: tag
     }
     window.Telegram.WebApp.sendData(JSON.stringify(post));
 };
 
+React.useEffect(() => {
+  const savedTags = localStorage.getItem('tags');
+  if (savedTags) {
+    setTag(savedTags);
+  }
+}, []);
+
+React.useEffect(() => {
+  localStorage.setItem('tags', tag);
+}, [tag]);
+// useEffect(() => {
+//   // Assumendo che window.Telegram.WebApp.initData sia disponibile
+//   if (window.Telegram && window.Telegram.WebApp) {
+//     window.Telegram.WebApp.showAlert(window.Telegram.WebApp.initData)
+//     setInitData(window.Telegram.WebApp.initData);
+//   }
+// }, []);
+
   return (
     <>
       <div className="container">
+      {/* Casella della community
+      <input
+        type="text"
+        placeholder={initData}
+        className="input-community"
+        value={initData}
+        onChange={(e) => setInitData(e.target.value)}
+      /> */}
       {/* Casella di input per il titolo */}
       <input
         type="text"
@@ -39,32 +62,24 @@ function App() {
       />
       {/* Casella di input per la descrizione */}
       <textarea
-        placeholder="Scrivi qui il corpo del post"
+        placeholder="Scrivi qui il corpo del post (Max 150)"
         className="input-description"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
+        maxLength={150}
       />
       {/* Casella di input per i tag */}
       <input
         type="text"
-        placeholder="Scrivi qui i tag"
+        placeholder="Scrivi qui i tag exaple: steem steemit steemexclusive"
         className="input-tag"
         value={tag}
-        onChange={(e) => setTag(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Write here account"
-        className="input-account"
-        value={account}
-        onChange={(e) => setAccount(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Write here password"
-        className="input-password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={(e) => {
+          const inputWords = e.target.value.split(' ');
+          if (inputWords.length <= 7) {
+            setTag(e.target.value);
+          }
+        }}
       />
       {/* Bottone di invio post */}
       <button className="button" onClick={inviaMessaggio}>Invia Post</button>
